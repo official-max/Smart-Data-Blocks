0699f0d253998a0a1f0fa0fb9e27a54d64a739bf
official-max <grtjatinofficial@gmail.com>
2025-06-09 13:04:34 +0530

    Update admin.js
    Improve Optimization

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,10 +1,14 @@
-// // \admin\assets\admin.js
+// \admin\assets\js\admin.js
 jQuery(document).ready(function ($) {
-
     console.log('admin.js loaded');
 
+    /*********************************************************************************************************
+    ************************************************ Groups JS ************************************************
+    *********************************************************************************************************/
+
+    // Load values based on selected location type (param)
     function loadValues($paramSelect, $valueSelect) {
-        var param = $paramSelect.val();
+        let param = $paramSelect.val();
 
         $.ajax({
             url: ajaxurl,
@@ -25,24 +29,25 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    // on param change
-    $(document).on('change', '.sdb-param-select', function () {
-        var $param = $(this);
-        var $value = $param.closest('.sdb-location-rule').find('.sdb-value-select');
+    // Update value dropdown when param changes
+    $('#sdb-location-rules').on('change', '.sdb-param-select', function () {
+        let $param = $(this);
+        let $value = $param.closest('.sdb-location-rule').find('.sdb-value-select');
         loadValues($param, $value);
     });
 
-    // Load initial for first rule
+
+    // Load values for existing rules on page load
     $('.sdb-location-rule').each(function () {
-        var $param = $(this).find('.sdb-param-select');
-        var $value = $(this).find('.sdb-value-select');
+        let $param = $(this).find('.sdb-param-select');
+        let $value = $(this).find('.sdb-value-select');
         loadValues($param, $value);
     });
 
-    // Add rule button (same as before, remember to initialize)
-    var index = 1;
+    // Add new location rule and load values
+    let index = 1;
     $('#add-location-rule').click(function () {
-        var html = `<div class="sdb-location-rule" style="margin-top:10px;">
+        let html = `<div class="sdb-location-rule" style="margin-top:10px;">
             <select name="location[${index}][param]" class="sdb-param-select">
                 <option value="post_type">Post Type</option>
                 <option value="post">Page / Post</option>
@@ -50,19 +55,17 @@ jQuery(document).ready(function ($) {
             </select>
             <select name="location[${index}][value]" class="sdb-value-select"></select>
         </div>`;
+
         $('#sdb-location-rules').append(html);
-        // Load values for new rule
-        var $newParam = $('#sdb-location-rules').find('.sdb-location-rule').last().find('.sdb-param-select');
-        var $newValue = $('#sdb-location-rules').find('.sdb-location-rule').last().find('.sdb-value-select');
+
+        let $newParam = $('#sdb-location-rules').find('.sdb-location-rule').last().find('.sdb-param-select');
+        let $newValue = $('#sdb-location-rules').find('.sdb-location-rule').last().find('.sdb-value-select');
         loadValues($newParam, $newValue);
         index++;
     });
 
-
-
-
-
-        document.querySelectorAll('[data-slug-target]').forEach(input => {
+    // Auto-generate slug from input
+    document.querySelectorAll('[data-slug-target]').forEach(input => {
         input.addEventListener('input', () => {
             const targetSelector = input.getAttribute('data-slug-target');
             const target = document.querySelector(targetSelector);
@@ -72,13 +75,11 @@ jQuery(document).ready(function ($) {
             const slug = input.value
                 .toLowerCase()
                 .trim()
-                .replace(/[^a-z0-9]+/g, '_')   // use _ instead of -
-                .replace(/^_+|_+$/g, '');       // trim leading/trailing _
+                .replace(/[^a-z0-9]+/g, '_')
+                .replace(/^_+|_+$/g, '');
 
             target.value = appendId ? `${slug}_${appendId}` : slug;
         });
     });
 
 });
-
-
